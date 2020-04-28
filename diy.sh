639a2b4c4a2b882c8fbb59b0e7f5d4bac79f4159
lovepaopao <380535324@qq.com>
2020-04-28 19:41:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,7 +11,14 @@ sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_gene
 
 
 #ssr
-#https://github.com/fw876/helloworld.git
+#git clone https://github.com/fw876/helloworld.git
+#去广告
+#git clone https://github.com/jefferymvp/luci-app-koolproxyR.git
+#主题
+#git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+#git clone https://github.com/rosywrt/luci-theme-rosy.git
+
 #./scripts/feeds clean
 #./scripts/feeds update -a
 #./scripts/feeds install -a
+#make menuconfig
